dffdb9a77b8db64aa85944aa65f29504b403f827
Chris Sanchez <c.sanch7@gmail.com>
2021-08-01 15:08:54 -0700

    add automapper and LogUserActivity

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers
+{
+  public class AutoMapperProfiles : Profile
+  {
+    public AutoMapperProfiles()
+    {
+      CreateMap<AppUser, MemberDto>();
+    }
+  }
+}
