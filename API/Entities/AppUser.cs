33b11b20c4ecd5d2ff385ca1ba97e3907e140c39
Chris Sanchez <c.sanch7@gmail.com>
2021-08-01 15:39:35 -0700

    update entity and dto and add first migration

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,9 +1,11 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
 namespace API.Entities
 {
-  public class AppUser
+  public class AppUser : IdentityUser<int>
   {
-    public int Id { get; set; }
-    public int Username { get; set; }
+    public string Username { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string City { get; set; }
@@ -11,5 +13,8 @@ namespace API.Entities
     public string Country { get; set; }
     public string HomeAirport { get; set; }
     public string FavoritePlane { get; set; }
+    public DateTime Created { get; set; } = DateTime.Now;
+    public DateTime LastActive { get; set; } = DateTime.Now;
+    public ICollection<AppUserRole> UserRoles { get; set; }
   }
 }
